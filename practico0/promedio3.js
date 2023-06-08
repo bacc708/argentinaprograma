5f38afebd5411f6baf427ef3ab1bd68db74db0a3
bacc708 <jmbaccei@gmail.com>
2023-06-08 20:46:02 -0300

    Practico N0 finalizado

---
+++ practico0/promedio3.js
@@ -0,0 +1,23 @@
+// promedio de 5, 7 y 9
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+let promedio = (num1 + num2 + num3)/3;
+console.log(promedio);
+// resultado = 7
+  
+// promedio de -2, -3 y -8
+num1 = -2;
+num2 = -3;
+num3 = -8;
+promedio = (num1 + num2 + num3)/3;
+console.log(promedio);
+// resultado = -4.333333333333333
+  
+// promedio de -1.2, 5.3 y -3.3
+num1 = -1.2;
+num2 = 5.3;
+num3 = -3.3;
+promedio = (num1 + num2 + num3)/3;
+console.log(promedio);
+// resultado = 0.2666666666666666
