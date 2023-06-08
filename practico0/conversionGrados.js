c74e73a69dab28e6f991da73134b827b04ca2be5
bacc708 <jmbaccei@gmail.com>
2023-06-08 20:41:57 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,11 @@
+// conversión de 50 grados F a C
+let fahrenheit = 50;
+let celsius = (fahrenheit - 32) * (5/9) ;
+console.log(celsius + " grados Celsius");
+// resultado = 10 grados Celsius
+  
+// conversión de 75 grados F a C
+fahrenheit = 75;
+celsius = (fahrenheit - 32) * (5/9) ;
+console.log(celsius + " grados Celsius");
+// resultado = 23.88888888888889 grados Celsius
